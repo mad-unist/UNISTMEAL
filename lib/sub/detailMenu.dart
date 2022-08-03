967bbfa82780ab5375175d99a124943afa3f5a17
woojin7879 <woojin7879@unist.ac.kr>
2022-08-04 04:38:48 +0900

    new functions

--- lib/sub/detailMenu.dart
+++ lib/sub/detailMenu.dart
@@ -15,7 +15,7 @@ class _detailMenuState extends State<detailMenu> {
   final Restaurant shopName;
   _detailMenuState(this.shopName);
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
+  int currentIndex = 0;
   List<String>? urlList = [];
 
   final imageList = [
@@ -34,6 +34,12 @@ class _detailMenuState extends State<detailMenu> {
     super.dispose();
   }
 
+  void onPageChanged(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,35 +52,52 @@ class _detailMenuState extends State<detailMenu> {
         height: MediaQuery.of(context).size.height,
         margin: EdgeInsets.only(left: 15, right: 15),
         width: MediaQuery.of(context).size.width,
-        child: PhotoViewGallery.builder(
-          itemCount: urlList?.length,
-          builder: (context, index) {
-            return PhotoViewGalleryPageOptions(
-              imageProvider: NetworkImage(
-                urlList![index],
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            PhotoViewGallery.builder(
+              itemCount: urlList?.length,
+              builder: (context, index) {
+                return PhotoViewGalleryPageOptions(
+                  imageProvider: NetworkImage(
+                    urlList![index],
+                  ),
+                  minScale: PhotoViewComputedScale.contained * 0.8,
+                  maxScale: PhotoViewComputedScale.covered * 2,
+                );
+              },
+              scrollPhysics: BouncingScrollPhysics(),
+              backgroundDecoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Theme.of(context).canvasColor,
               ),
-              minScale: PhotoViewComputedScale.contained * 0.8,
-              maxScale: PhotoViewComputedScale.covered * 2,
-            );
-          },
-          scrollPhysics: BouncingScrollPhysics(),
-          backgroundDecoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            color: Theme.of(context).canvasColor,
-          ),
-          enableRotation: false,
-          loadingBuilder: (context, event) => Center(
-            child: Container(
-              width: 30.0,
-              height: 30.0,
-              child: CircularProgressIndicator(
-                backgroundColor: Colors.orange,
-                value: event == null
-                    ? 0
-                    : 0,
+              enableRotation: false,
+              onPageChanged: onPageChanged,
+              loadingBuilder: (context, event) => Center(
+                child: Container(
+                  width: 30.0,
+                  height: 30.0,
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.orange,
+                    value: event == null
+                        ? 0
+                        : 0,
+                  ),
+                ),
               ),
             ),
-          ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "${currentIndex + 1} / ${urlList?.length}",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: MediaQuery.of(context).size.width * 0.05,
+                  decoration: null,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
